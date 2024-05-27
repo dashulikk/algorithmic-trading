1a6cd9ce298c6b0c80e852113bcbb0be282a05b7
ubuntu <daryna.havrada@gmail.com>
2024-05-27 23:00:40 +0200

    Added base trading bot

--- trading_bots/base_trading_bot.py
+++ trading_bots/base_trading_bot.py
@@ -1,6 +1,6 @@
 import abc
 
-from utils.broker_connector import authenticate
+from utils.broker_connector import authenticate, topup, create_user
 from utils.broker_response_parser import parse_auth_token
 
 
@@ -9,7 +9,12 @@ class BaseTradingBot(abc.ABC):
         self.username = username
         self.password = password
 
+        create_user(username, password) # will not create a user if it already exists
+
         self.auth_token = parse_auth_token(authenticate(username, password))
+
+    def topup(self, amount: float):
+        topup(self.auth_token, amount)
 
     @abc.abstractmethod
     def run(self):
