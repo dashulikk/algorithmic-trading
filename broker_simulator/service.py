70a4f67d34cf3b9bb5abc6bee16768a8e520aece
ubuntu <daryna.havrada@gmail.com>
2024-05-27 17:17:06 +0200

    Added methods for order processing

--- broker_simulator/service.py
+++ broker_simulator/service.py
@@ -42,7 +42,7 @@ class Service:
     def stock_price(stock: str) -> float:
         return get_stock_price(stock)
 
-    def buy_stock(self, username: str, stock: str, amount: float) -> None:
+    def buy_stock(self, username: str, stock: str, amount: float, commit=True) -> None:
         if amount <= 0:
             raise ServiceException(f"Amount has to be positive. Amount provided: {amount}")
 
@@ -51,9 +51,9 @@ class Service:
         total = stock_price * amount
 
         fee = self._calculate_fee(total)
-        self.db.buy_stock(username, stock, amount, total, fee)
+        self.db.buy_stock(username, stock, amount, total, fee, commit=commit)
 
-    def sell_stock(self, username: str, stock: str, amount: float) -> None:
+    def sell_stock(self, username: str, stock: str, amount: float, commit=True) -> None:
         if amount <= 0:
             raise ServiceException(f"Amount has to be positive. Amount provided: {amount}")
 
@@ -62,7 +62,7 @@ class Service:
         total = stock_price * amount
 
         fee = self._calculate_fee(total)
-        self.db.sell_stock(username, stock, amount, total, fee)
+        self.db.sell_stock(username, stock, amount, total, fee, commit=commit)
 
     def get_net_worth(self, username: str) -> float:
         cash = self.get_balance(username)
@@ -80,3 +80,36 @@ class Service:
     def get_order_book(self) -> list[Order]:
         return self.db.get_all_orders()
 
+    @staticmethod
+    def can_execute_order(order: Order) -> bool:
+        curr_stock_price = Service.stock_price(order.stock)
+        trigger_stock_price = order.trigger_price
+
+        if order.order_type == "limit" or order.order_type == "stop_loss":
+            return curr_stock_price <= trigger_stock_price
+        elif order.order_type == "take_profit":
+            return curr_stock_price >= trigger_stock_price
+        else:
+            raise ServiceException(f"Order is of type {order.order_type}, "
+                                   f"Only limit, stop_loss and take_profit orders are accepted")
+
+    def execute_order(self, order: Order) -> None:
+        if order.order_type == "limit":
+            try:
+                self.buy_stock(order.username, order.stock, order.amount, commit=False)
+                self.db.delete_order(order.id, commit=False)
+                self.db.conn.commit()
+            except Exception as e:
+                self.db.conn.rollback()
+                raise ServiceException(f"{e}")
+        elif order.order_type == "stop_loss" or order.order_type == "take_profit":
+            try:
+                self.sell_stock(order.username, order.stock, order.amount, commit=False)
+                self.db.delete_order(order.id, commit=False)
+                self.db.conn.commit()
+            except Exception as e:
+                self.db.conn.rollback()
+                raise ServiceException(f"{e}")
+        else:
+            raise ServiceException(f"Order is of type {order.order_type}, "
+                                   f"Only limit, stop_loss and take_profit orders are accepted")
